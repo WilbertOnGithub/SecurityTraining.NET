062cc72697115bac50aca86e6d3243ca944c6247
Wilbert van Dolleweerd <wilbert@arentheym.com>
2017-10-26 21:25:41 +0200

    Add default admin role and admin user

--- SecurityTraining/Startup.cs
+++ SecurityTraining/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SecurityTraining.Models;
 
 [assembly: OwinStartupAttribute(typeof(SecurityTraining.Startup))]
 namespace SecurityTraining
@@ -9,6 +12,42 @@ namespace SecurityTraining
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            CreateDefaultRolesAndUsers();
+        }
+
+        private void CreateDefaultRolesAndUsers()                                                                      
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            const string admin = "Admin";
+
+            if (!roleManager.RoleExists(admin))
+            {
+
+                // first we create Admin rool   
+                var role = new IdentityRole { Name = admin };
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser
+                {
+                    UserName = admin,
+                    Email = "wilbert@arentheym.com"
+                };
+
+                IdentityResult result = UserManager.Create(user, admin);
+
+                //Add default User to Role Admin   
+                if (result.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, admin);
+                }
+            }
         }
     }
 }
