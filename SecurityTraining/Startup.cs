dc4bbe6d72e3601f4237c4cce23640d1b5b3ae90
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2017-10-26 20:47:51 +0200

    Add project files.

---
+++ SecurityTraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityTraining.Startup))]
+namespace SecurityTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
