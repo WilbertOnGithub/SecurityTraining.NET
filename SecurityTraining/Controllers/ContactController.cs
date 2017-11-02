9bf8f6b4c5b19e97499199bb1a5c0221ce3c4a8b
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2017-11-02 16:30:21 +0100

    WIP

---
+++ SecurityTraining/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using SecurityTraining.Models;
+
+namespace SecurityTraining.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult AddContactToCustomer()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Sales")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddContactToCustomer(AddOrderForContactViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            string currentUserId = User.Identity.GetUserId();
+            ApplicationUser currentUser = context.Users.FirstOrDefault(x => x.Id == currentUserId);
+
+            context.Customers.Add(new Customer {Name = model.Name, SalesPerson = currentUser});
+
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ SecurityTraining/Models/AddContactForCustomerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SecurityTraining.Models
+{
+    public class AddContactForCustomerViewModel
+    {
+        [Required]
+        [Display(Name = "Naam")]
+        public string Name { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public IEnumerable<SelectListItem> Customers { get; set; }
+    }
+}
