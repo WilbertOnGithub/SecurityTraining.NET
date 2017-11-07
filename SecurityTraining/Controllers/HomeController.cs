8f721c238519845eb94227df1a5ff534b3fe24e6
Wilbert van Dolleweerd <wilbert@arentheym.com>
2017-11-07 20:36:04 +0100

    Remove demo viewmodels, controllers and views
    Remove unnecessary code not needed for the casus of Security Training
    .NET

--- SecurityTraining/Controllers/HomeController.cs
+++ SecurityTraining/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SecurityTraining.Controllers
 {
@@ -15,7 +11,7 @@ namespace SecurityTraining.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Security .NET training";
 
             return View();
         }
