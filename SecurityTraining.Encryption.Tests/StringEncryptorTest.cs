001486bf94042c0344aa3108b57a9fde4582c218
Wilbert van Dolleweerd <wilbert.van.dolleweerd@ict.nl>
2017-11-09 15:55:01 +0100

    Add utility class for encryption

---
+++ SecurityTraining.Encryption.Tests/StringEncryptorTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SecurityTraining.Encryption.Tests
+{
+    [TestFixture]
+    public class StringEncryptorTest
+    {
+        [Test]
+        public void StringEncryptor_GivenCorrectKey_DecryptsMessage()
+        {
+            string plainText = "Mary had a little lamb.";
+            string key = "Behold, I've become the destroyer of worlds.";
+
+            string encryptedText = StringEncryptor.Encrypt(plainText, key);
+
+            string decryptedText = StringEncryptor.Decrypt(encryptedText, key);
+
+            decryptedText.Should().Be(plainText, "the encryptor decrypted the encrypted test.");
+        }
+
+        [Test]
+        public void StringEncryptor_GivenIncorrectKey_CannotDecryptMessage()
+        {
+            string plainText = "Mary had a little lamb.";
+            string key = "Behold, I've become the destroyer of worlds.";
+
+            string encryptedText = StringEncryptor.Encrypt(plainText, key);
+
+            Action act = () => StringEncryptor.Decrypt(encryptedText, "wrong key");
+
+            act.ShouldThrow<CryptographicException>();
+        }
+    }
+}
+
